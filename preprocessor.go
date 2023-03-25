fc208e82d866fa8382577d4517c1f002754c71ea
RussellLuo <luopeng.he@gmail.com>
2023-03-25 16:21:26 +0800

    Add Preprocessor

---
+++ preprocessor.go
@@ -0,0 +1,201 @@
+package gptbot
+
+import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+
+	"github.com/go-aie/xslices"
+	"github.com/google/uuid"
+	tokenizer "github.com/samber/go-gpt-3-encoder"
+)
+
+type PreprocessorConfig struct {
+	// ChunkTokenNum is the number of tokens for each text chunk.
+	// Defaults to 200.
+	ChunkTokenNum int
+
+	// MinChunkCharNum is the minimum number of characters for each text chunk.
+	// Defaults to 350.
+	MinChunkCharNum int
+
+	// MinChunkLenToEmbed is the minimum length in characters.
+	// Chunks with shorter length will be discarded.
+	//
+	// Defaults to 5.
+	MinChunkLenToEmbed int
+
+	// MaxChunkNum is the maximum number of chunks to generate from a text.
+	// Defaults to 10000.
+	MaxChunkNum int
+}
+
+func (cfg *PreprocessorConfig) init() *PreprocessorConfig {
+	if cfg.ChunkTokenNum == 0 {
+		cfg.ChunkTokenNum = 200
+	}
+	if cfg.MinChunkCharNum == 0 {
+		cfg.MinChunkCharNum = 350
+	}
+	if cfg.MinChunkLenToEmbed == 0 {
+		cfg.MinChunkLenToEmbed = 5
+	}
+	if cfg.MaxChunkNum == 0 {
+		cfg.MaxChunkNum = 10000
+	}
+	return cfg
+}
+
+// Preprocessor splits a list of documents into chunks.
+type Preprocessor struct {
+	encoder *dummyTokenizer
+	cfg     *PreprocessorConfig
+}
+
+func NewPreprocessor(cfg *PreprocessorConfig) (*Preprocessor, error) {
+	encoder, err := newDummyTokenizer()
+	if err != nil {
+		return nil, err
+	}
+	return &Preprocessor{
+		encoder: encoder,
+		cfg:     cfg.init(),
+	}, nil
+}
+
+func (p *Preprocessor) Preprocess(docs ...*Document) (map[string][]*Chunk, error) {
+	chunkMap := make(map[string][]*Chunk)
+
+	for _, doc := range docs {
+		docID := doc.ID
+		if docID == "" {
+			docID = uuid.New().String()
+		}
+
+		textChunks, err := p.split(doc.Text)
+		if err != nil {
+			return nil, err
+		}
+
+		for i, textChunk := range textChunks {
+			chunkMap[docID] = append(chunkMap[docID], &Chunk{
+				ID:         fmt.Sprintf("%s_%d", docID, i),
+				Text:       textChunk,
+				DocumentID: docID,
+			})
+		}
+	}
+
+	return chunkMap, nil
+}
+
+// split converts the text into chunks.
+//
+// The splitting algorithm is borrowed from https://github.com/openai/chatgpt-retrieval-plugin/blob/88d983585816b7f298edb0cabf7502c5ccff370d/services/chunks.py#L22-L96.
+func (p *Preprocessor) split(text string) ([]string, error) {
+	if text == "" || strings.TrimSpace(text) == "" {
+		return nil, nil
+	}
+
+	// Convert the document text into runes.
+	runes := []rune(text)
+
+	var chunks []string
+
+	var i int
+	var chunkNum int
+
+	for i < len(runes) && chunkNum < p.cfg.MaxChunkNum {
+		// Take the first ChunkTokenNum tokens as a chunk.
+		chunkRunes, err := p.encoder.Encode(runes[i:], p.cfg.ChunkTokenNum)
+		if err != nil {
+			return nil, nil
+		}
+
+		// Skip the chunk if it is empty or whitespace.
+		chunkText := string(chunkRunes)
+		if chunkText == "" || strings.TrimSpace(chunkText) == "" {
+			i += len(chunkRunes)
+			continue
+		}
+
+		// Find the last period or punctuation mark in the chunk.
+		// Note that here we count the index in runes.
+		var lastPuncIdx = -1
+		for _, punc := range []rune{
+			'.', '?', '!',
+			'。', '？', '！',
+			'\n',
+		} {
+			lastPuncIdx = xslices.Max(lastPuncIdx, lastRuneIndex(chunkText, punc))
+		}
+
+		if lastPuncIdx != -1 && lastPuncIdx > p.cfg.MinChunkCharNum {
+			// Truncate the chunk text at the punctuation mark.
+			chunkText = string([]rune(chunkText)[:lastPuncIdx+1])
+		}
+
+		trimmedChunkText := strings.TrimSpace(strings.ReplaceAll(chunkText, "\n", " "))
+		if utf8.RuneCountInString(trimmedChunkText) > p.cfg.MinChunkLenToEmbed {
+			chunks = append(chunks, trimmedChunkText)
+		}
+
+		i += utf8.RuneCountInString(chunkText)
+		chunkNum += 1
+	}
+
+	// Handle the remaining runes.
+	if i < len(runes) {
+		remainingText := string(runes[i:])
+		trimmedRemainingText := strings.TrimSpace(strings.ReplaceAll(remainingText, "\n", " "))
+		if utf8.RuneCountInString(trimmedRemainingText) > p.cfg.MinChunkLenToEmbed {
+			chunks = append(chunks, trimmedRemainingText)
+		}
+	}
+
+	return chunks, nil
+}
+
+func lastRuneIndex(s string, r rune) int {
+	runes := []rune(s)
+	for i := len(runes) - 1; i >= 0; i-- {
+		if runes[i] == r {
+			return i
+		}
+	}
+	return -1
+}
+
+// dummyTokenizer tokenizes any given string at the rune level, but counts the
+// number of tokens as correctly as possible by using go-gpt-3-encoder.
+//
+// The reason why we do not use go-gpt-3-encoder directly is that it can not
+// handle Chinese characters properly.
+type dummyTokenizer struct {
+	encoder *tokenizer.Encoder
+}
+
+func newDummyTokenizer() (*dummyTokenizer, error) {
+	encoder, err := tokenizer.NewEncoder()
+	if err != nil {
+		return nil, err
+	}
+	return &dummyTokenizer{encoder: encoder}, nil
+}
+
+// Encode iterates through runes and returns a slice of the leading runes, which
+// consume at most tokenNum number of tokens.
+func (t *dummyTokenizer) Encode(runes []rune, tokenNum int) ([]rune, error) {
+	b := strings.Builder{}
+	for i, r := range runes {
+		_, _ = b.WriteRune(r)
+		tokens, err := t.encoder.Encode(b.String())
+		if err != nil {
+			return nil, err
+		}
+		if len(tokens) > tokenNum {
+			return runes[:i], nil
+		}
+	}
+	return runes, nil
+}
---
+++ preprocessor_test.go
@@ -0,0 +1,73 @@
+package gptbot_test
+
+import (
+	"testing"
+
+	"github.com/go-aie/gptbot"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestPreprocessor_Preprocess(t *testing.T) {
+	p, err := gptbot.NewPreprocessor(&gptbot.PreprocessorConfig{
+		ChunkTokenNum:   150,
+		MinChunkCharNum: 50,
+	})
+	if err != nil {
+		t.Fatalf("err: %v\n", err)
+	}
+
+	tests := []struct {
+		in   []*gptbot.Document
+		want map[string][]*gptbot.Chunk
+	}{
+		{
+			in: []*gptbot.Document{
+				{
+					ID:   "1",
+					Text: "Generative Pre-trained Transformer 3 (GPT-3) is an autoregressive language model released in 2020 that uses deep learning to produce human-like text. Given an initial text as prompt, it will produce text that continues the prompt.\n\nThe architecture is a decoder-only transformer network with a 2048-token-long context and then-unprecedented size of 175 billion parameters, requiring 800GB to store. The model was trained using generative pre-training; it is trained to predict what the next token is based on previous tokens. The model demonstrated strong zero-shot and few-shot learning on many tasks.[2]",
+				},
+				{
+					ID:   "2",
+					Text: "生成型预训练变换模型 3 （英语：Generative Pre-trained Transformer 3，简称 GPT-3）是一个自回归语言模型，目的是为了使用深度学习生成人类可以理解的自然语言[1]。GPT-3是由在旧金山的人工智能公司OpenAI训练与开发，模型设计基于谷歌开发的 Transformer 语言模型。GPT-3的神经网络包含1750亿个参数，需要800GB来存储, 为有史以来参数最多的神经网络模型[2]。该模型在许多任务上展示了强大的零样本和少样本的能力。[3]",
+				},
+			},
+			want: map[string][]*gptbot.Chunk{
+				"1": {
+					{
+						ID:         "1_0",
+						Text:       "Generative Pre-trained Transformer 3 (GPT-3) is an autoregressive language model released in 2020 that uses deep learning to produce human-like text. Given an initial text as prompt, it will produce text that continues the prompt.  The architecture is a decoder-only transformer network with a 2048-token-long context and then-unprecedented size of 175 billion parameters, requiring 800GB to store. The model was trained using generative pre-training; it is trained to predict what the next token is based on previous tokens. The model demonstrated strong zero-shot and few-shot learning on many tasks.",
+						DocumentID: "1",
+					},
+				},
+				"2": {
+					{
+						ID:         "2_0",
+						Text:       "生成型预训练变换模型 3 （英语：Generative Pre-trained Transformer 3，简称 GPT-3）是一个自回归语言模型，目的是为了使用深度学习生成人类可以理解的自然语言[1]。",
+						DocumentID: "2",
+					},
+					{
+						ID:         "2_1",
+						Text:       "GPT-3是由在旧金山的人工智能公司OpenAI训练与开发，模型设计基于谷歌开发的 Transformer 语言模型。",
+						DocumentID: "2",
+					},
+					{
+						ID:         "2_2",
+						Text:       "GPT-3的神经网络包含1750亿个参数，需要800GB来存储, 为有史以来参数最多的神经网络模型[2]。该模型在许多任务上展示了强大的零样本和少样本的能力。",
+						DocumentID: "2",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		got, err := p.Preprocess(tt.in...)
+		if err != nil {
+			t.Errorf("err: %v\n", err)
+		}
+
+		if !cmp.Equal(got, tt.want) {
+			diff := cmp.Diff(got, tt.want)
+			t.Errorf("Want - Got: %s", diff)
+		}
+	}
+}
