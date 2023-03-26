4f001a19eac1d906526131ad1e929b6ed4dbd52e
RussellLuo <luopeng.he@gmail.com>
2023-03-26 20:43:34 +0800

    Add Gradio UI

--- cmd/gptbot/README.md
+++ cmd/gptbot/README.md
@@ -5,16 +5,15 @@
 
 Install and run the Milvus server (see [instructions](../../milvus)).
 
-
-## Quick Start
-
-Start the GPTBot server:
+## Start GPTBot Server
 
 ```bash
 $ go build
 $ ./gptbot
 2023/03/25 22:48:13 transport=HTTP addr=:8080
 ```
+
+## Using cURL
 
 Upload a document file:
 
@@ -27,3 +26,22 @@ Chat with the bot:
 ```bash
 $ curl -H 'Content-Type: application/json' http://localhost:8080/chat -d '{"question": "When was GPT-3 introduced in the paper?"}'
 ```
+
+## Using Gradio
+
+Install dependencies:
+
+```bash
+$ cd gradio
+$ python3 -m venv venv
+$ source venv/bin/activate
+$ pip3 install -r requirements.txt
+```
+
+Run the UI:
+
+```bash
+$ python3 ui.py
+```
+
+![gradio](gradio/gradio.png)
---
+++ cmd/gptbot/gradio/ui.py
@@ -0,0 +1,76 @@
+import gradio as gr
+import requests
+
+
+URL = 'http://127.0.0.1:8080'
+
+
+def handle_error(resp):
+    if resp.status_code != 200:
+        raise gr.Error(resp.json()['error']['message'])
+
+
+def create(text):
+    resp = requests.post(URL+'/upsert', json=dict(documents=[dict(text=text)]))
+    handle_error(resp)
+    return 'Created successfully!'
+
+
+def upload(file):
+    resp = requests.post(URL+'/upload', files=dict(file=file))
+    handle_error(resp)
+    return 'Uploaded successfully!'
+
+
+def clear():
+    resp = requests.post(URL+'/delete', json=dict(document_ids=[]))
+    handle_error(resp)
+    return 'Cleared successfully!'
+
+
+def chat(question):
+    resp = requests.post(URL+'/chat', json=dict(question=question))
+    handle_error(resp)
+    return resp.json()['answer']
+
+
+with gr.Blocks() as demo:
+    with gr.Tab('Chat Bot'):
+        chatbot = gr.Chatbot()
+        msg = gr.Textbox(label='Input')
+        btn = gr.Button('Clear')
+
+        question = ''
+
+        def user(msg, history):
+            global question
+            question = msg
+            return '', history + [[question, None]]
+
+        def bot(history):
+            answer = chat(question)
+            history[-1][1] = answer
+            return history
+
+        msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
+            bot, [chatbot], chatbot
+        )
+        btn.click(lambda: None, None, chatbot, queue=False)
+
+    with gr.Tab('Knowledge Base'):
+        state = gr.Textbox(label='state')
+        btn = gr.Button(value="Clear All")
+        btn.click(clear, inputs=None, outputs=[state])
+
+        with gr.Tab('Document Text'):
+            text = gr.Textbox(label='Document Text', lines=8)
+            btn = gr.Button(value="Create")
+            btn.click(create, inputs=[text], outputs=[state])
+        with gr.Tab('Document File'):
+            file = gr.File(label='Document File')
+            btn = gr.Button(value="Upload")
+            btn.click(upload, inputs=[file], outputs=[state])
+
+
+if __name__ == '__main__':
+    demo.launch()
