f04075bbe45e4d9165ca5b212a3b803c7cb8b064
Clark Than <lenplex@gmail.com>
2023-04-11 18:28:31 +0800

    add both /v1/chat/completions and /v1/completions api support

--- bot.go
+++ bot.go
@@ -7,6 +7,22 @@ import (
 
 	"github.com/rakyll/openai-go"
 	"github.com/rakyll/openai-go/chat"
+	"github.com/rakyll/openai-go/completion"
+)
+
+type ModelType string
+
+const (
+	// GPT-4
+	GPT4 ModelType = "gpt-4"
+	// GPT-3.5
+	GPT3Dot5Turbo  ModelType = "gpt-3.5-turbo"
+	TextDavinci003 ModelType = "text-davinci-003"
+	TextDavinci002 ModelType = "text-davinci-002"
+	// GPT-3
+	TextAda001     ModelType = "text-ada-001"
+	TextCurie001   ModelType = "text-curie-001"
+	TextBabbage001 ModelType = "text-babbage-001"
 )
 
 type Encoder interface {
@@ -39,7 +55,7 @@ type BotConfig struct {
 
 	// Model is the ID of OpenAI's model to use for chat.
 	// Defaults to "gpt-3.5-turbo".
-	Model string
+	Model ModelType
 
 	// PromptTmpl specifies a custom prompt template.
 	// Defaults to DefaultPromptTmpl.
@@ -48,34 +64,49 @@ type BotConfig struct {
 	// TopK specifies how many candidate similarities will be selected to construct the prompt.
 	// Defaults to 3.
 	TopK int
+
+	// The maximum number of tokens to generate in the completion
+	// Defaults to 256
+	MaxTokens int
 }
 
 func (cfg *BotConfig) init() {
 	if cfg.Model == "" {
-		cfg.Model = "gpt-3.5-turbo"
+		cfg.Model = GPT3Dot5Turbo
 	}
 	if cfg.TopK == 0 {
 		cfg.TopK = 3
+	}
+	if cfg.MaxTokens == 0 {
+		cfg.MaxTokens = 256
 	}
 	if cfg.PromptTmpl == "" {
 		cfg.PromptTmpl = DefaultPromptTmpl
 	}
 }
 
-// Bot is a chatbot powered by OpenAI's GPT models.
 type Bot struct {
-	client *chat.Client
-	cfg    *BotConfig
+	cfg        *BotConfig
+	chatClient *chat.Client
+	compClient *completion.Client
 }
 
 func NewBot(cfg *BotConfig) *Bot {
 	cfg.init()
 	s := openai.NewSession(cfg.APIKey)
+	bot := &Bot{cfg: cfg}
 
-	return &Bot{
-		client: chat.NewClient(s, cfg.Model),
-		cfg:    cfg,
+	// https://platform.openai.com/docs/models/model-endpoint-compatibility
+	switch cfg.Model {
+	case GPT4, GPT3Dot5Turbo:
+		bot.chatClient = chat.NewClient(s, string(cfg.Model))
+	case TextDavinci003, TextDavinci002, TextAda001, TextBabbage001, TextCurie001:
+		bot.compClient = completion.NewClient(s, string(cfg.Model))
+	default:
+		panic("unsupported gpt model!")
 	}
+
+	return bot
 }
 
 type Conversation interface {
@@ -84,11 +115,20 @@ type Conversation interface {
 }
 
 func (b *Bot) Chat(ctx context.Context, question string, history ...*Turn) (string, error) {
-	prompt, err := b.constructPrompt(ctx, question)
+	prompt, err := b.cfg.constructPrompt(ctx, question)
 	if err != nil {
 		return "", err
 	}
 
+	if b.chatClient != nil {
+		return b.doChatCompletion(ctx, question, prompt, history...)
+	}
+
+	return b.doCompletion(ctx, question, prompt, history...)
+}
+
+// powered by /v1/chat/completions completion api, supported model like `gpt-3.5-turbo`
+func (b *Bot) doChatCompletion(ctx context.Context, question, prompt string, history ...*Turn) (string, error) {
 	var messages []*chat.Message
 	for _, h := range history {
 		messages = append(messages, &chat.Message{
@@ -105,7 +145,10 @@ func (b *Bot) Chat(ctx context.Context, question string, history ...*Turn) (stri
 		Content: prompt,
 	})
 
-	resp, err := b.client.CreateCompletion(ctx, &chat.CreateCompletionParams{Messages: messages})
+	resp, err := b.chatClient.CreateCompletion(ctx, &chat.CreateCompletionParams{
+		MaxTokens: b.cfg.MaxTokens,
+		Messages:  messages,
+	})
 	if err != nil {
 		return "", err
 	}
@@ -114,13 +157,28 @@ func (b *Bot) Chat(ctx context.Context, question string, history ...*Turn) (stri
 	return answer, nil
 }
 
-func (b *Bot) constructPrompt(ctx context.Context, question string) (string, error) {
-	emb, err := b.cfg.Encoder.Encode(ctx, question)
+// powered by /v1/completions completion api, supported model like `text-davinci-003`
+func (b *Bot) doCompletion(ctx context.Context, question, prompt string, history ...*Turn) (string, error) {
+	resp, err := b.compClient.Create(ctx, &completion.CreateParams{
+		MaxTokens: b.cfg.MaxTokens,
+		Prompt:    []string{prompt},
+	})
 	if err != nil {
 		return "", err
 	}
 
-	similarities, err := b.cfg.Querier.Query(ctx, emb, b.cfg.TopK)
+	answer := resp.Choices[0].Text
+
+	return answer, nil
+}
+
+func (b *BotConfig) constructPrompt(ctx context.Context, question string) (string, error) {
+	emb, err := b.Encoder.Encode(ctx, question)
+	if err != nil {
+		return "", err
+	}
+
+	similarities, err := b.Querier.Query(ctx, emb, b.TopK)
 	if err != nil {
 		return "", err
 	}
@@ -130,7 +188,7 @@ func (b *Bot) constructPrompt(ctx context.Context, question string) (string, err
 		texts = append(texts, s.Text)
 	}
 
-	p := PromptTemplate(b.cfg.PromptTmpl)
+	p := PromptTemplate(b.PromptTmpl)
 	return p.Render(PromptData{
 		Question: question,
 		Sections: texts,
