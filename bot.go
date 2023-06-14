98f10d14227ef7c6c71030a1bcae1ab79932afef
Clark Than <lenplex@gmail.com>
2023-06-14 13:51:52 +0800

    Abstract various Chat API implementations as engines (#5)
    * abstract the AI chat api implementation into Engine
    
    * add missing logic
    
    * update some api design
    
    * move Engine interface into bot.go
    
    * change placement of Encoder and Querier

--- bot.go
+++ bot.go
@@ -5,35 +5,7 @@ import (
 	"context"
 	"strings"
 	"text/template"
-
-	"github.com/rakyll/openai-go"
-	"github.com/rakyll/openai-go/chat"
-	"github.com/rakyll/openai-go/completion"
 )
-
-type ModelType string
-
-const (
-	// GPT-4
-	GPT4 ModelType = "gpt-4"
-	// GPT-3.5
-	GPT3Dot5Turbo  ModelType = "gpt-3.5-turbo"
-	TextDavinci003 ModelType = "text-davinci-003"
-	TextDavinci002 ModelType = "text-davinci-002"
-	// GPT-3
-	TextAda001     ModelType = "text-ada-001"
-	TextCurie001   ModelType = "text-curie-001"
-	TextBabbage001 ModelType = "text-babbage-001"
-)
-
-type Encoder interface {
-	Encode(cxt context.Context, text string) (Embedding, error)
-	EncodeBatch(cxt context.Context, texts []string) ([]Embedding, error)
-}
-
-type Querier interface {
-	Query(ctx context.Context, embedding Embedding, corpusID string, topK int) ([]*Similarity, error)
-}
 
 // Turn represents a round of dialogue.
 type Turn struct {
@@ -42,9 +14,13 @@ type Turn struct {
 }
 
 type BotConfig struct {
-	// APIKey is the OpenAI's APIKey.
+	// APIKey is the LLM Platform's APIKey.
 	// This field is required.
 	APIKey string
+
+	// Engine is the LLM Platform api implementation
+	// Defaults to OpenAI's chat api which using gpt-3.5-turbo model
+	Engine Engine
 
 	// Encoder is an Embedding Encoder, which will encode the user's question into a vector for similarity search.
 	// This field is required.
@@ -113,28 +89,47 @@ func (cfg *BotConfig) init() {
 	if cfg.MultiTurnPromptTmpl == "" {
 		cfg.MultiTurnPromptTmpl = DefaultMultiTurnPromptTmpl
 	}
+	if cfg.Engine == nil {
+		cfg.Engine = NewOpenAIChatEngine(cfg.APIKey, cfg.Model)
+	}
+}
+
+type EngineMessage struct {
+	Role    string `json:"role,omitempty"`
+	Content string `json:"content,omitempty"`
+}
+
+type EngineRequest struct {
+	Messages    []*EngineMessage `json:"messages,omitempty"`
+	Temperature float64          `json:"temperature,omitempty"`
+	MaxTokens   int              `json:"max_tokens,omitempty"`
+}
+
+type EngineResponse struct {
+	Text string `json:"text,omitempty"`
+}
+
+type Engine interface {
+	Infer(context.Context, *EngineRequest) (*EngineResponse, error)
+}
+
+type Encoder interface {
+	Encode(cxt context.Context, text string) (Embedding, error)
+	EncodeBatch(cxt context.Context, texts []string) ([]Embedding, error)
+}
+
+type Querier interface {
+	Query(ctx context.Context, embedding Embedding, corpusID string, topK int) ([]*Similarity, error)
 }
 
 type Bot struct {
-	cfg        *BotConfig
-	chatClient *chat.Client
-	compClient *completion.Client
+	cfg *BotConfig
 }
 
+// NewBot support single and multi-turn chat request
 func NewBot(cfg *BotConfig) *Bot {
 	cfg.init()
-	s := openai.NewSession(cfg.APIKey)
 	bot := &Bot{cfg: cfg}
-
-	// https://platform.openai.com/docs/models/model-endpoint-compatibility
-	switch cfg.Model {
-	case GPT4, GPT3Dot5Turbo:
-		bot.chatClient = chat.NewClient(s, string(cfg.Model))
-	case TextDavinci003, TextDavinci002, TextAda001, TextBabbage001, TextCurie001:
-		bot.compClient = completion.NewClient(s, string(cfg.Model))
-	default:
-		panic("unsupported gpt model!")
-	}
 
 	return bot
 }
@@ -211,45 +206,17 @@ func (b *Bot) singleTurnChat(ctx context.Context, question string, opts *chatOpt
 }
 
 func (b *Bot) chat(ctx context.Context, prompt string, temperature float64) (string, error) {
-	if b.chatClient != nil {
-		return b.doChatCompletion(ctx, prompt, temperature)
-	}
-	return b.doCompletion(ctx, prompt, temperature)
-}
-
-// powered by /v1/chat/completions completion api, supported model like `gpt-3.5-turbo`
-func (b *Bot) doChatCompletion(ctx context.Context, prompt string, temperature float64) (string, error) {
-	resp, err := b.chatClient.CreateCompletion(ctx, &chat.CreateCompletionParams{
-		Messages: []*chat.Message{
-			{
-				Role:    "user",
-				Content: prompt,
-			},
-		},
+	req := &EngineRequest{
+		Messages:    []*EngineMessage{{Role: "user", Content: prompt}},
 		Temperature: temperature,
 		MaxTokens:   b.cfg.MaxTokens,
-	})
+	}
+	resp, err := b.cfg.Engine.Infer(ctx, req)
 	if err != nil {
 		return "", err
 	}
 
-	answer := resp.Choices[0].Message.Content
-	return answer, nil
-}
-
-// powered by /v1/completions completion api, supported model like `text-davinci-003`
-func (b *Bot) doCompletion(ctx context.Context, prompt string, temperature float64) (string, error) {
-	resp, err := b.compClient.Create(ctx, &completion.CreateParams{
-		Prompt:      []string{prompt},
-		Temperature: temperature,
-		MaxTokens:   b.cfg.MaxTokens,
-	})
-	if err != nil {
-		return "", err
-	}
-
-	answer := resp.Choices[0].Text
-	return answer, nil
+	return resp.Text, nil
 }
 
 func (b *BotConfig) constructPrompt(ctx context.Context, question string, opts *chatOptions) (string, error) {
--- engine.go
+++ engine.go
@@ -2,33 +2,91 @@ package gptbot
 
 import (
 	"context"
+
+	"github.com/rakyll/openai-go"
+	"github.com/rakyll/openai-go/chat"
+	"github.com/rakyll/openai-go/completion"
 )
 
-type EngineMessage struct {
-	Role    string `json:"role,omitempty"`
-	Content string `json:"content,omitempty"`
-}
+type ModelType string
 
-type EngineRequest struct {
-	Messages    []*EngineMessage `json:"messages,omitempty"`
-	Temperature float64          `json:"temperature,omitempty"`
-	MaxTokens   int              `json:"max_tokens,omitempty"`
-}
+const (
+	// GPT-4
+	GPT4     ModelType = "gpt-4"
+	GPT40301 ModelType = "gpt-4-0314"
 
-type EngineResponse struct {
-	Text string `json:"text,omitempty"`
-}
+	// GPT-3.5
+	GPT3Dot5Turbo     ModelType = "gpt-3.5-turbo"
+	GPT3Dot5Turbo0301 ModelType = "gpt-3.5-turbo-0301"
+	TextDavinci003    ModelType = "text-davinci-003"
+	TextDavinci002    ModelType = "text-davinci-002"
+	// GPT-3
+	TextAda001     ModelType = "text-ada-001"
+	TextCurie001   ModelType = "text-curie-001"
+	TextBabbage001 ModelType = "text-babbage-001"
+)
 
-type Engine interface {
-	Infer(context.Context, *EngineRequest) (*EngineResponse, error)
-}
+// https://platform.openai.com/docs/models/model-endpoint-compatibility
 
+// OpenAIChatEngine powered by /v1/chat/completions completion api, supported model like:
+// `gpt-4`, `gpt-4-0314`, `gpt-3.5-turbo`, `gpt-3.5-turbo-0301` ...
 type OpenAIChatEngine struct {
-	// TODO: Implement this engine.
-	// gpt-4, gpt-4-0314, gpt-4-32k, gpt-4-32k-0314, gpt-3.5-turbo, gpt-3.5-turbo-0301
+	Client *chat.Client
 }
 
+// OpenAICompletionEngine powered by /v1/completions completion api, supported model like:
+// `text-davinci-003`, `text-davinci-002`, `text-ada-001`, `text-curie-001`, `text-babbage-001` ...
 type OpenAICompletionEngine struct {
-	// TODO: Implement this engine.
-	// text-davinci-003, text-davinci-002, text-curie-001, text-babbage-001, text-ada-001
+	Client *completion.Client
+}
+
+func NewOpenAIChatEngine(apiKey string, model ModelType) *OpenAIChatEngine {
+	client := chat.NewClient(openai.NewSession(apiKey), string(model))
+
+	return &OpenAIChatEngine{
+		Client: client,
+	}
+}
+
+func NewOpenAICompletionEngine(apiKey string, model ModelType) *OpenAICompletionEngine {
+	compClient := completion.NewClient(openai.NewSession(apiKey), string(model))
+
+	return &OpenAICompletionEngine{
+		Client: compClient,
+	}
+}
+
+func (e *OpenAIChatEngine) Infer(ctx context.Context, req *EngineRequest) (resp *EngineResponse, err error) {
+	apiResp, err := e.Client.CreateCompletion(ctx, &chat.CreateCompletionParams{
+		Messages: []*chat.Message{
+			{
+				Role:    req.Messages[0].Role,
+				Content: req.Messages[0].Content,
+			},
+		},
+		Temperature: req.Temperature,
+		MaxTokens:   req.MaxTokens,
+	})
+	if err != nil {
+		return
+	}
+
+	return &EngineResponse{
+		Text: apiResp.Choices[0].Message.Content,
+	}, nil
+}
+
+func (e *OpenAICompletionEngine) Infer(ctx context.Context, req *EngineRequest) (resp *EngineResponse, err error) {
+	apiResp, err := e.Client.Create(ctx, &completion.CreateParams{
+		Prompt:      []string{req.Messages[0].Content},
+		Temperature: req.Temperature,
+		MaxTokens:   req.MaxTokens,
+	})
+	if err != nil {
+		return
+	}
+
+	return &EngineResponse{
+		Text: apiResp.Choices[0].Text,
+	}, nil
 }
